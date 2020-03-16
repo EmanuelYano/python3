9fb6e3c783dd10c828f5d1cfdb06544f63671b24
emanuel <netodaveia2110@gmail.com>
2020-03-15 23:07:09 -0400

    att

---
+++ algo2.py
@@ -0,0 +1,10 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> vet = []
+>>> for i in range(5)
+SyntaxError: invalid syntax
+>>> for i in range(5):
+	vet = int(input())
+
--- numsDif.py
+++ numsDif.py
@@ -2,40 +2,82 @@
 #-*- coding: utf-8 -*-
 a = int(input())
 neg,imp,pos,par = 0,0,0,0
-if a < 0 and a%2 != 0:
+if a < 0:
 	neg += 1
-	imp += 1
-else:
+	if a % 2 != 0:
+		imp += 1
+	else:
+		par += 1
+elif a > 0:
 	pos += 1
+	if a % 2 == 0:
+		par += 1
+	else:
+		imp += 1
+else:
 	par += 1
 b = int(input())
-if b < 0 and b%2 != 0:
+if b < 0:
 	neg += 1
-	imp += 1
-else:
+	if b % 2 != 0:
+		imp += 1
+	else:
+		par += 1
+elif b > 0:
 	pos += 1
+	if b % 2 == 0:
+		par += 1
+	else:
+		imp += 1
+else:
 	par += 1
 c = int(input())
-if c < 0 and c%2 != 0:
+if c < 0:
 	neg += 1
-	imp += 1
-else:
+	if c % 2 != 0:
+		imp += 1
+	else:
+		par += 1
+elif c > 0:
 	pos += 1
+	if c % 2 == 0:
+		par += 1
+	else:
+		imp += 1
+else:
 	par += 1
 d = int(input())
-if d < 0 and d%2 != 0:
+if d < 0:
 	neg += 1
-	imp += 1
-else:
+	if d % 2 != 0:
+		imp += 1
+	else:
+		par += 1
+elif d > 0:
 	pos += 1
+	if d % 2 == 0:
+		par += 1
+	else:
+		imp += 1
+else:
 	par += 1
 e = int(input())
-if e < 0 and e%2 != 0:
+if e < 0:
 	neg += 1
-	imp += 1
+	if e % 2 != 0:
+		imp += 1
+	else:
+		par += 1
+elif e > 0:
+	pos += 1
+	if e % 2 == 0:
+		par += 1
+	else:
+		imp += 1
 else:
 	pos += 1
-	par += 1
-
-print(par,imp,"\n",pos,neg)
+print("%d valor(es) par(es)"%(par))
+print("%d valor(es) impar(es)"%(imp)) 
+print("%d valor(es) positivo(s)"%(pos)) 
+print("%d valor(es) negativo(s)"%(neg))
 exit(0)
